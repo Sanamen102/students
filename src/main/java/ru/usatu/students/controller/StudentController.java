426f4711d17f2bef79aeffa7b1765913e8bf8a5c
pozid <pozidaev2002@gmail.com>
2022-11-07 18:06:52 +0500

    По приколу

---
+++ src/main/java/ru/usatu/students/controller/StudentController.java
@@ -0,0 +1,39 @@
+package ru.usatu.students.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.usatu.students.model.Student;
+import ru.usatu.students.service.StudentService;
+import ru.usatu.students.service.StudentServiceXml;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private StudentServiceXml studentService;
+
+    public StudentController(StudentServiceXml studentService) {
+        this.studentService= studentService;
+    }
+    @GetMapping
+    public @ResponseBody
+    List<Student> getStudents() throws Exception {
+        return studentService.getStudents(); }
+    @GetMapping("/{id}")
+    public @ResponseBody
+    Student getStudent(@PathVariable int id) throws Exception {return studentService.getStudent(id);}
+
+    @PostMapping
+    public @ResponseBody
+    Student addStudent(@RequestBody Student student) throws Exception {return studentService.addStudent(student);}
+
+    @PutMapping("/{id}")
+    public @ResponseBody
+    Student editStudent(@PathVariable int id, @RequestBody String name) throws Exception {return studentService.editStudent(id,name);}
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteStudent(@PathVariable int id) throws Exception { studentService.deleteStudent(id);}
+}
---
+++ src/main/java/ru/usatu/students/model/Student.java
@@ -0,0 +1,38 @@
+package ru.usatu.students.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Objects;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Student {
+    @XmlElement(name="id")
+    private int id;
+    @XmlElement(name="name")
+    private String name;
+
+    public Student() {}
+
+    public Student (int id, String name) {
+        this.id=id;
+        this.name=name;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id=id; }
+    public String getName() {return name;}
+    public void setName(String name) {this.name=name;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {return Objects.hash(id);}
+
+}
