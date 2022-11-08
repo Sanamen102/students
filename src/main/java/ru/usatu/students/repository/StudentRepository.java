05acb0138d293dfc3f55a2ca5f8df815c0a2f35e
pozid <pozidaev2002@gmail.com>
2022-11-08 12:34:48 +0500

    Лаба 3

---
+++ src/main/java/ru/usatu/students/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package ru.usatu.students.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.usatu.students.model.Student;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {}
+
